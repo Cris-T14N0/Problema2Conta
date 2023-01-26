e9e282faf344cca8f6d2be6fafc89681dfb37dfd
Cris-T14N0 <cristianonunesoliveira2006@gmail.com>
2023-01-26 18:09:13 +0000

    First WIP

---
+++ conta.php
@@ -0,0 +1,28 @@
+<?php
+
+$nome = $_POST['nome'];
+$saldo = $_POST['saldo'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <p> <h1>
+
+    <?php echo "Bem-vindo ", $nome, "!"; ?>
+
+    </h1> <p> <br>
+
+    <h2>
+    <?php echo "O seu saldo: ", $saldo, "€"; ?>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form action="conta.php" method="post">
+
+        <p> <h1>
+
+        <label>Criar conta</label>
+
+        </h1> <p> <br> 
+        
+        <h3>
+        <label>Insira o seu nome: </label>
+        <input type="text" name = "nome">
+
+        </p>
+
+        <label>Insira o seu saldo (€)</label>
+        <input type="number" name = "saldo">
+
+        <input type = "submit" name = "Criar conta">
+
+    </form>
+
+</h3>
+</body>
+</html>
